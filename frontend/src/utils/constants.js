ea480024efc7729cba14cfa29e877450a0e1a177
shrashi <shreya.upadhyaya1506@gmail.com>
2022-03-29 13:59:30 +0530

    corrected filename errors

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,22 @@
+export const carouselItems = [
+  {
+    src: "../../assets/carousel-item1.jpg",
+    alt: "first slide",
+  },
+  {
+    src: "../../assets/carousel-item2.jpg",
+    alt: "second slide",
+  },
+  {
+    src: "../../assets/carousel-item.jpg",
+    alt: "third slide",
+  },
+  {
+    src: "../../assets/carousel-item1.jpg",
+    alt: "fourth slide",
+  },
+  {
+    src: "../../assets/carousel-item1.jpg",
+    alt: "fifth slide",
+  },
+];
