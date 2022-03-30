bf623e5431f464b41ba073dbf00b9cdf73f8ef31
shrashi <shreya.upadhyaya1506@gmail.com>
2022-03-30 14:28:59 +0530

    added carousel indicator

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,22 +1,39 @@
+import React from "react";
+import img1 from "../assets/carousel-item1.jpg";
+import img2 from "../assets/carousel-item2.jpg";
+import img3 from "../assets/carousel-item3.jpg";
+import img4 from "../assets/carousel-item4.jpg";
+import img5 from "../assets/carousel-item5.jpg";
+
 export const carouselItems = [
   {
-    src: "../../assets/carousel-item1.jpg",
-    alt: "first slide",
+    src: img1,
+    alt: "first-carousel-item",
+    imageNo: 0,
+    url: "/url1",
   },
   {
-    src: "../../assets/carousel-item2.jpg",
-    alt: "second slide",
+    src: img2,
+    alt: "second-carousel-item",
+    imageNo: 1,
+    url: "/url2",
   },
   {
-    src: "../../assets/carousel-item.jpg",
-    alt: "third slide",
+    src: img3,
+    alt: "third-carousel-item",
+    imageNo: 2,
+    url: "url3",
   },
   {
-    src: "../../assets/carousel-item1.jpg",
-    alt: "fourth slide",
+    src: img4,
+    alt: "fourth-carousel-item",
+    imageNo: 3,
+    url: "/url3",
   },
   {
-    src: "../../assets/carousel-item1.jpg",
-    alt: "fifth slide",
+    src: img5,
+    alt: "fifth-carousel-item",
+    imageNo: 4,
+    url: "/url4",
   },
 ];
